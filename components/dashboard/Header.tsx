77dab49a344336a1f2fea73559c24946ea22e192
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2022-04-18 16:11:42 -0600

    add bottom nav on mobile

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -1,18 +1,26 @@
-import headerStyles from "../../styles/header.module.css"
+import headerStyles from "../../styles/header.module.scss"
 import Link from "next/link"
  
 export default function Header(props: {name: string}) {
     return (
-        <div className="position-fixed bg-primary shadow h-100 text-white p-2 d-none d-md-block">
+        <div className={`${headerStyles.wrapper} bg-primary`} /* className="position-fixed bg-primary shadow h-100 text-white p-2 d-none d-md-block" */>
             <header>
-                <p className="text-center">{props.name}&apos;s dashboard</p>
+                <p className="text-center text-white d-none d-md-block">{props.name}&apos;s dashboard</p>
             </header>
-            <nav className={`${headerStyles.header} text-white d-md-flex flex-md-column align-items-center gap-2 w-15`}>
-                <ul className="list-group w-100">
-                    <li className="list-group-item text-center bg-primary border-0 w-100"><Link href="/">
-                        <a className="w-100"><img src='/assets/images/icons/chart-line.svg' alt="line graph" style={{width: "3rem"}}/></a>
+            <nav className={`${headerStyles.header} text-white d-flex flex-md-column align-items-center`}>
+                <ul className="w-100 d-flex flex-md-column">
+                    <li className="list-group-item text-center border-0 w-100"><Link href="/">
+                        <a className="w-100" aria-label="investments"><img src='/assets/images/icons/chart-line.svg' alt="line graph" style={{width: "3rem"}}/></a>
                     </Link></li>
-
+                    <li className="list-group-item text-center border-0 w-100"><Link href='/'>
+                        <a className="w-100" aria-label="news"><img src="/assets/images/icons/newspaper.svg" alt="newspaper" style={{width: "3rem"}}/></a>
+                    </Link></li>
+                    <li className="list-group-item text-center border-0 w-100"><Link href='/'>
+                        <a className="w-100" aria-label="my pie"><img src="/assets/images/icons/chart-pie.svg" alt="pie chart" style={{width: "3rem"}}/></a>
+                    </Link></li>
+                    <li className="list-group-item text-center border-0 w-100"><Link href='/'>
+                        <a className="w-100" aria-label="settings"><img src="/assets/images/icons/gear.svg" alt="gear" style={{width: "3rem"}}/></a>
+                    </Link></li>
                 </ul>
                 
                 {/* <FontAwesomeIcon className={`${headerStyles.focus} text-primary w-100`} icon={faChartLine}/> */}
--- pages/index.tsx
+++ pages/index.tsx
@@ -93,5 +93,4 @@ const Home: NextPage = () => {
     </>
   )
 }
-
 export default Home
